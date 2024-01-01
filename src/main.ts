1af2375e9413281d324f4b58c88bf055221398c5
HoangChien201 <hoangchien11522@gmail.com>
2024-01-02 00:41:43 +0700

    thirdly commit

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.enableCors()
   console.log(process.env.PORT);
   
-  await app.listen(process.env.PORT);
+  await app.listen(parseInt(process.env.PORT));
 
   if (module.hot) {
     module.hot.accept();
